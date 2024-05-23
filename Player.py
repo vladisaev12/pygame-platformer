152003e6220548c591ef6f39acf6b9d5d956ee59
vladisaev12 <isaev.vladislav.e.2012@gmail.com>
2024-05-23 17:58:06 +0300

    added a second charachter

--- Player.py
+++ Player.py
@@ -13,8 +13,8 @@ class Player(BaseEntity):
         self.minspd = self.speed * 0.15 # 15% of speed is minimal
         self.aPK = {} # (A)lready (P)ressed (K)eys
         self.idle_anim = []
-        for i in range(1, 13):
-            self.idle_anim.append(image.load(getcwd() + f"/animations/player/idle{i}.png"))
+        for i in range(1, 9):
+            self.idle_anim.append(image.load(getcwd() + f"/animations/player2/idle//idle{i}.png"))
         self.cur_anim = "idle"
         self.cur_frame = 0
     #y is inversed since pygame positioning is top-left to bottom-right
@@ -67,7 +67,7 @@ class Player(BaseEntity):
             self.aPK[key] = False
         # draw.rect(self.screen, self.color, rect.Rect(self.x, self.y, self.size, self.size))
         if self.cur_anim == "idle":
-            if self.cur_frame < 11:
+            if self.cur_frame < 7:
                 self.cur_frame += 1
             else:
                 self.cur_frame = 0
